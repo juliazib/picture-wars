f96fcfc1e97e0585be44975196c8cbe8e3b974e3
juliazib <julia.zibordi@gmail.com>
2022-05-25 18:53:01 -0300

    Estilização da página.

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-// When someone sends a date input, an image or video must show in the screen, followed by a title and description. 
-// To make the video appear, I need to make and XML HTTP request and  use the NASA APOD API
-// To make the elements appear, I will need to change the source of the image or video on my page and add text to the text elements on HTML.
-// To do all that, I must use jQuery. 
-// Dates are shown like this yyyy-mm-dd and they are strings
-
 $("#ver").click(function() {
     let date = $("#date").val()
     
@@ -12,7 +6,6 @@ $("#ver").click(function() {
         type: "GET",
 
         success: function(response) {
-            console.log(response)
             $("#title").text(response.title)
             $("#image").attr("src", response.url)
             $("#explanation").text(response.explanation)
@@ -21,7 +14,9 @@ $("#ver").click(function() {
                 $("#video").show()
                 $("#video").attr("src", response.url)
             }
-
+            else if (response.media_type == "image") {
+                $("#video").hide()
+            }
         },
 
         error: function() {
